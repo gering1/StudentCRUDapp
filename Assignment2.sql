582ed004b9e219cba86b40b346bec3796e4f0bba
Colton Gering <42728082+gering1@users.noreply.github.com>
2020-03-22 15:15:22 -0700

    Add files via upload

---
+++ Assignment2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Student (StudentId INTEGER PRIMARY KEY,
+                        FirstName VarChar(32),
+                        LastName VarChar(32),
+                        GPA numeric,
+                        Major VarChar(32),
+                        FacultyAdvisor varchar(32),
+                        isDeleted integer
+
+);
